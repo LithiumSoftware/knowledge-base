3e921024774fbdb30008582eec15fdbc91410a5f
DogChef <garciageronimo@outlook.com>
2020-03-27 14:12:59 -0300

    nested editors now work. Added a button to open the menu

--- packages/mobile/App.tsx
+++ packages/mobile/App.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createDrawerNavigator } from "@react-navigation/drawer";
@@ -7,19 +6,10 @@ import {
   Provider as PaperProvider,
   Theme
 } from "react-native-paper";
-=======
-import * as React from "react";
-import { Button, StyleSheet, Text, View, TextInput } from "react-native";
-import { DefaultTheme, Provider as PaperProvider } from "react-native-paper";
-import { Theme } from "react-native-paper";
-import { NavigationContainer, useNavigation } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
->>>>>>> b3e3019ca1d8079464a0fbedad270d9ff9a725dd
 import { ApolloClient, ApolloClientOptions } from "apollo-client";
 import { ApolloProvider } from "@apollo/react-hooks";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { HttpLink } from "apollo-link-http";
-import { apollo } from "@workspace-library/core";
 
 import HomeScreen from "./screens/Home";
 
@@ -29,165 +19,40 @@ const theme: Theme = {
   colors: {
     ...DefaultTheme.colors,
     primary: "#3498db",
-    accent: "#f1c40f",
-  },
-};
-
-<<<<<<< HEAD
-=======
-const { Navigator, Screen } = createStackNavigator();
-
-function HomeScreen({ route }: { route: any }) {
-  React.useEffect(() => {
-    if (route.params?.post) {
-      // Post updated, do something with `route.params.post`
-      // For example, send the post to the server
-    }
-  }, [route.params?.post]);
-  const navigation = useNavigation();
-
-  return (
-    <View style={styles.container}>
-      <Text style={{ textAlign: "center" }}>
-        Hello! This is the first template from where we are going to work!
-      </Text>
-      <Button
-        title="Go to Details"
-        onPress={() =>
-          navigation.navigate("Details", {
-            parameter: "you can pass a parameter here",
-          })
-        }
-      />
-      {/* Pass data to previous screen: */}
-      <Button title="Create post" onPress={() => navigation.navigate("CreatePost")} />
-      <Text style={{ textAlign: "center", color: "red" }}>Post: {route.params?.post}</Text>
-      <Text style={{ textAlign: "center" }}>Lithium team</Text>
-    </View>
-  );
-}
-
-function DetailsScreen({ route, navigation }) {
-  const { parameter } = route.params;
-
-  return (
-    <View style={styles.container}>
-      <Text style={{ textAlign: "center" }}>Details Screen</Text>
-      <Text style={{ textAlign: "center", color: "red" }}>{parameter}</Text>
-      <Button
-        title="Go to Details... again (navigate)"
-        onPress={() =>
-          navigation.navigate("Details", {
-            parameter: parameter,
-          })
-        }
-      />
-      <Button
-        title="Go to Details... again (push)"
-        onPress={() =>
-          navigation.push("Details", {
-            parameter:
-              parameter == "and change it on each call"
-                ? "this is good"
-                : parameter == "this is good"
-                ? parameter
-                : "and change it on each call",
-          })
-        }
-      />
-      <Button title="Go to Home (navigate)" onPress={() => navigation.navigate("Home")} />
-      <Button title="Go back" onPress={() => navigation.goBack()} />
-      <Button title="Go back to first screen in stack" onPress={() => navigation.popToTop()} />
-    </View>
-  );
-}
-
-function CreatePostScreen({ navigation }) {
-  const [postText, setPostText] = React.useState("");
-
-  return (
-    <>
-      <TextInput
-        multiline
-        placeholder="What's on your mind?"
-        style={{ height: 200, padding: 10, backgroundColor: "white" }}
-        value={postText}
-        onChangeText={setPostText}
-      />
-      <Button
-        title="Done"
-        onPress={() => {
-          // Pass params back to home screen
-          navigation.navigate("Home", { post: postText });
-        }}
-      />
-    </>
-  );
-}
-
->>>>>>> b3e3019ca1d8079464a0fbedad270d9ff9a725dd
-const cache = new InMemoryCache();
-const link = new HttpLink({
-  uri: "http://localhost:4000/",
-});
-
-const Drawer = createDrawerNavigator(
-  {
-    Home: {
-      screen: MyHomeScreen
-    }
-  },
-  {
-    initialRouteName: "Home"
+    accent: "#f1c40f"
   }
-);
+};
 
 /*const test = <Icon
   name="bars"
   style={{ color: "white", padding: 10, marginLeft: 10, fontSize: 20 }}
 />*/
 
-export default function App() {
-  return (
-    <ApolloProvider client={apollo}>
-      <PaperProvider theme={theme}>
-        <NavigationContainer>
-<<<<<<< HEAD
-          <Drawer.Navigator initialRouteName="Home">
-            <Drawer.Screen
-              name="Home"
-              component={HomeScreen}
-              options={{
-                title: "Welcome"
-=======
-          <Navigator>
-            <Screen name="Home" component={HomeScreen} />
-            <Screen
-              name="Details"
-              component={DetailsScreen}
-              initialParams={{
-                parameter:
-                  "Message shown when you didn't specify any params when navigating to the screen",
->>>>>>> b3e3019ca1d8079464a0fbedad270d9ff9a725dd
-              }}
-            />
-          </Drawer.Navigator>
-        </NavigationContainer>
-      </PaperProvider>
-    </ApolloProvider>
-  );
-}
-<<<<<<< HEAD
-=======
-
-const styles = StyleSheet.create({
-  container: {
-    display: "flex",
-    flex: 1,
-    padding: 30,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "space-evenly",
-  },
+const client: ApolloClient<{}> = new ApolloClient({
+  ssrMode: typeof window === "undefined", // Disables forceFetch on the server (so queries are only run once)
+  link: new HttpLink({
+    uri: "http://localhost:3000/api/graphql", // Server URL (must be absolute)
+    credentials: "same-origin", // Additional fetch() options like `credentials` or `headers`
+    fetch
+  }),
+  cache: new InMemoryCache()
 });
->>>>>>> b3e3019ca1d8079464a0fbedad270d9ff9a725dd
+
+const Drawer = createDrawerNavigator();
+
+export default () => (
+  <ApolloProvider client={client}>
+    <PaperProvider theme={theme}>
+      <NavigationContainer>
+        <Drawer.Navigator drawerType="front">
+          <Drawer.Screen
+            name="Welcome"
+            component={HomeScreen}
+            initialParams={{ list: ["One", "Two", "Three", "Four", "Five"] }}
+          />
+        </Drawer.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
+  </ApolloProvider>
+);
+
--- packages/mobile/screens/Home.tsx
+++ packages/mobile/screens/Home.tsx
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { createStackNavigator } from "@react-navigation/stack";
-
 import {
   Image,
   ScrollView,
@@ -10,65 +9,122 @@ import {
   TouchableOpacity,
   View
 } from "react-native";
+import { Button } from "react-native-paper";
 
-const Stack = createStackNavigator();
-
-const DrawerButton = props => {
+/*const StackButton = props => {
   return (
     <View>
       <TouchableOpacity
         onPress={() => {
-          props.navigation.navigate("DrawerOpen");
+          props.navigation.navigate("StackOpen");
         }}
       >
         <Image source={require("../img/iconImage.png")} style={styles.icon} />
       </TouchableOpacity>
     </View>
   );
-};
+};*/
 
-const HomeScreen = props => (
-  <Stack.Navigator initialRouteName="Home">
+/*const StackNavigatorConfig = {
+  options: {
+    header: (props: any) => <AppBar {...props} searchable />,
+    headerStyle: {
+      backgroundColor: "transparent"
+    },
+    gesturesEnabled: false
+  }
+};*/
+
+const Stack = createStackNavigator();
+//
+const HomeScreen = ({ navigation, route }) => (
+  <Stack.Navigator
+    initialRouteName="Home"
+    mode="modal"
+    headerMode="float"
+    screenOptions={{
+      headerStyle: { backgroundColor: "#FFC200" },
+      headerLeft: () => (
+        <Button
+          style={{ minWidth: 0, left: 4 }}
+          icon="menu"
+          onPress={() => navigation.toggleDrawer()}
+        />
+      ),
+      headerTitleContainerStyle: { left: 44 }
+    }}
+  >
     <Stack.Screen
       name="Home"
       component={HomeComponent}
-      initialParams={{ ...props }}
+      initialParams={{ ...route.params }}
       options={{
-        title: "Welcome!",
-        headerStyle: {
-          backgroundColor: "#f4511e"
-        },
-        headerTintColor: "#fff",
-        headerTitleStyle: {
-          fontWeight: "bold"
-        }
+        title: "Lithium KB App - Friday 27/03 Demo"
       }}
     />
   </Stack.Navigator>
 );
 
+/*options = {{
+  header: (props: any) => <HomeComponent {...props} searchable />
+}}*/
+
 const HomeComponent = props => {
-  const filteredTodos = ["One", "Two", "Three", "Four", "Five"];
+  console.log(props);
+  const filteredTodos = [
+    "one",
+    "two",
+    "three",
+    "one",
+    "two",
+    "three",
+    "one",
+    "two",
+    "three"
+  ];
 
   return (
     <View style={styles.main}>
-      <View style={styles.todoList}>
-        {filteredTodos.map(todo => (
-          <Text> {todo} </Text>
-        ))}
-      </View>
+      <Text>
+        {" "}
+        Es muy importante para nosotros que se tomen este desafío muy
+        seriamente, que se enfoquen en desarrollar dicha tarea de manera que el
+        código generado pueda ser re-utilizado en el futuro (esto último, es muy
+        importante para Lithium ya que nuestra rapidez de delivery de soluciones
+        depende de ello), cumpla con las mejores prácticas, comentarios
+        pertinentes y justificación de decisiones tomadas
+      </Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   main: {
-    margin: 16,
+    display: "flex",
+    flex: 1,
+    width: "100%",
+    flexDirection: "column",
+    padding: 18,
+    paddingTop: 8,
+    paddingBottom: 8,
     alignItems: "center",
-    justifyContent: "center"
+    backgroundColor: "#fff"
   },
+
   todoList: {
-    backgroundColor: "white"
+    flex: 1,
+    width: "100%",
+    backgroundColor: "red"
+  },
+  todoListG: {
+    flex: 1,
+    width: "100%",
+    backgroundColor: "#FFC200"
+  },
+  todoListB: {
+    flex: 1,
+    width: "100%",
+    backgroundColor: "green"
   }
 });
 
