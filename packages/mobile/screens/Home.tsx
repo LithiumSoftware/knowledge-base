a3f4a1924cd864e5da1c2dd58a813ddc5738b11d
DogChef <garciageronimo@outlook.com>
2020-04-07 18:02:47 -0300

    Added an article query @HomeScreen screen

--- packages/mobile/screens/Home.tsx
+++ packages/mobile/screens/Home.tsx
@@ -1,19 +1,29 @@
 import React, { Component } from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { useArticlesQuery } from "@workspace-library/core";
 
-export default HomeScreen = ({ navigation, route }) => (
-  <View style={styles.main}>
-    <Text>
-      {" "}
-      Es muy importante para nosotros que se tomen este desafío muy seriamente,
-      que se enfoquen en desarrollar dicha tarea de manera que el código
-      generado pueda ser re-utilizado en el futuro (esto último, es muy
-      importante para Lithium ya que nuestra rapidez de delivery de soluciones
-      depende de ello), cumpla con las mejores prácticas, comentarios
-      pertinentes y justificación de decisiones tomadas
-    </Text>
-  </View>
-);
+const HomeScreen = ({ navigation, route }: { navigation: any; route: any }) => {
+  const data = useArticlesQuery({
+    fetchPolicy: "no-cache",
+    notifyOnNetworkStatusChange: true,
+  });
+
+  return (
+    <View style={styles.main}>
+      <Text>
+        {" "}
+        Es muy importante para nosotros que se tomen este desafío muy
+        seriamente, que se enfoquen en desarrollar dicha tarea de manera que el
+        código generado pueda ser re-utilizado en el futuro (esto último, es muy
+        importante para Lithium ya que nuestra rapidez de delivery de soluciones
+        depende de ello), cumpla con las mejores prácticas, comentarios
+        pertinentes y justificación de decisiones tomadas
+      </Text>
+    </View>
+  );
+};
+
+export default HomeScreen;
 
 const styles = StyleSheet.create({
   main: {
@@ -25,22 +35,22 @@ const styles = StyleSheet.create({
     paddingTop: 8,
     paddingBottom: 8,
     alignItems: "center",
-    backgroundColor: "#fff"
+    backgroundColor: "#fff",
   },
 
   todoList: {
     flex: 1,
     width: "100%",
-    backgroundColor: "red"
+    backgroundColor: "red",
   },
   todoListG: {
     flex: 1,
     width: "100%",
-    backgroundColor: "#FFC200"
+    backgroundColor: "#FFC200",
   },
   todoListB: {
     flex: 1,
     width: "100%",
-    backgroundColor: "green"
-  }
+    backgroundColor: "green",
+  },
 });
