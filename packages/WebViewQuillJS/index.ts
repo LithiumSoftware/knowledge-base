9ca7593cdcb551bf5691b88d8c0c2a65c137f8d5
Bruno Pintos <bpintos@lithium.com.uy>
2020-04-13 11:21:38 -0300

    packages/WebViewQuillJS removed

--- packages/WebViewQuillJS/index.ts
+++
@@ -1,9 +0,0 @@
-export { default as WebViewQuillJS } from "./WebViewQuillJS";
-export {
-  WebviewQuillJSEvents,
-  WebviewQuillJSMessage,
-  WebViewQuillJSProps
-} from "./models";
-
-import WebViewQuillJS from "./WebViewQuillJS";
-export default WebViewQuillJS;
