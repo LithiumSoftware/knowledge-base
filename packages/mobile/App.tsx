b43fab005015df4ea6ee14356665a2bad0d28d7b
rodrigo28 <rbenoit@lithium.com.uy>
2020-04-15 11:22:42 -0300

    added home to navigation

--- packages/mobile/App.tsx
+++ packages/mobile/App.tsx
@@ -10,8 +10,10 @@ import { ApolloProvider } from "react-apollo";
 import { ApolloClient } from "apollo-client";
 import { HttpLink } from "apollo-link-http";
 import { createStackNavigator } from "@react-navigation/stack";
-import SignUp from "./screens/sign_up";
-import SignIn from "./screens/sign_in";
+import SignUp from "./screens/SignUp";
+import SignIn from "./screens/SignIn";
+import HomeScreen from "./screens/Home";
+
 import { InMemoryCache } from "apollo-cache-inmemory";
 
 import AppNavigator from "./navigation/AppNavigator";
@@ -27,7 +29,7 @@ const theme: Theme = {
 };
 
 const link = new HttpLink({
-  uri: "http://192.168.1.2:3000/api/graphql", // Server URL (must be absolute)
+  uri: "http://192.168.1.4:3000/api/graphql", // Server URL (must be absolute)
 });
 const cache = new InMemoryCache();
 
@@ -35,6 +37,8 @@ const client = new ApolloClient({
   link: link,
   cache: cache,
 });
+
+const { Navigator, Screen } = createStackNavigator();
 
 const MyApp = () => (
   <PaperProvider theme={theme}>
@@ -49,6 +53,10 @@ const MyApp = () => (
                   "Message shown when you didn't specify any params when navigating to the screen"
               }}/>
                <Screen name="SignIn" component={SignIn}  initialParams={{
+                parameter:
+                  "Message shown when you didn't specify any params when navigating to the screen"
+              }}/>
+               <Screen name="HomeScreen" component={HomeScreen}  initialParams={{
                 parameter:
                   "Message shown when you didn't specify any params when navigating to the screen"
               }}/>
